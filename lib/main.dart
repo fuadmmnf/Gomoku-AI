55ae6d6e2c8f390f2694211a563c0aa4928738b9
Fahmid Ahmed <bsse0921@iit.du.ac.bd>
2019-08-19 03:25:20 +0600

    board search implemented

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,22 @@ import 'package:flutter/material.dart';
 
 import 'pages/dashboard.dart';
 import 'pages/gamepage.dart';
-
+import 'utilies/board.dart';
 void main() => runApp(MyApp());
+
+//void main(){
+//  var brd=new BoardClass();
+//  brd.changeEntry(5, 6, 1);
+//  brd.changeEntry(6, 7, 1);
+//  brd.changeEntry(7, 8, 1);
+//  brd.changeEntry(8, 9, 1);
+//  brd.boardPrint();
+//  if(brd.searchForMatch(4, 1))
+//    print("Match Found");
+//  else
+//    print("No match found");
+//
+//}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
---
+++ lib/utilies/board.dart
@@ -0,0 +1,129 @@
+import 'dart:core';
+
+class BoardClass{
+
+  List <List<num>> board = new List.generate(10, (i) => new List(10));
+
+  BoardClass(){
+
+    for(int i=0;i<10;i++){
+     for(int j=0;j<10;j++){
+       board[i][j]=-1;
+     }
+    }
+
+  }
+
+  void changeEntry(int a, int b, int c){
+    board[a][b]=c;
+  }
+
+  void boardPrint(){
+    print(board);
+  }
+
+  bool searchRow(int n,int currentPlayer){
+
+    for(int row=0;row<10;row++){
+      for(int i=0;i<=10-n;i++){
+        if(board[row][i]==currentPlayer){
+          int j;
+          for(j=1;j<n;j++){
+            if(board[row][i]!=board[row][i+j]){
+              break;
+            }
+          }
+          if(j==n){
+            return true;
+          }
+        }
+      }
+    }
+
+    return false;
+  }
+
+
+  bool searchColumn(int n,int currentPlayer){
+
+    for(int col=0;col<10;col++){
+      for(int i=0;i<=10-n;i++){
+        if(board[i][col]==currentPlayer){
+          int j;
+          for(j=1;j<n;j++){
+            if(board[i][col]!=board[i+j][col]){
+              break;
+            }
+          }
+          if(j==n){
+            return true;
+          }
+        }
+      }
+    }
+
+    return false;
+  }
+
+
+  bool searchRightDiagonal(int n,int currentPlayer){
+
+    for(int row=0;row<10;row++){
+      for(int i=0;i<=10-n;i++){
+        if(board[row][i]==currentPlayer){
+          int j;
+          for(j=1;j<n;j++){
+            if(board[row][i]!=board[row+j][i+j]){
+              break;
+            }
+          }
+          if(j==n){
+            return true;
+          }
+        }
+      }
+    }
+
+    return false;
+  }
+
+
+  bool searchLeftDiagonal(int n,int currentPlayer){
+
+    for(int row=9;row>=0;row--){
+      for(int i=0;i<=10-n;i++){
+        if(board[row][i]==currentPlayer){
+          int j;
+          for(j=1;j<n;j++){
+            if(board[row][i]!=board[row-j][i+j]){
+              break;
+            }
+          }
+          if(j==n){
+            return true;
+          }
+        }
+      }
+    }
+
+    return false;
+  }
+
+
+
+  bool searchForMatch(int n,int currentPlayer){
+
+    if(searchRow(n,currentPlayer))
+     return true;
+    else if(searchColumn(n,currentPlayer))
+      return true;
+    else if(searchRightDiagonal(n,currentPlayer))
+      return true;
+    else if(searchLeftDiagonal(n,currentPlayer))
+      return true;
+
+    return false;
+
+  }
+
+}
