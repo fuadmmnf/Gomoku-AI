32b923acf3e117c7913ab983315b4b9d37aed5e4
fuadmmnf <bsse0920@iit.du.ac.bd>
2019-09-04 09:03:27 -0500

    testing ai evaluation function again

--- lib/utils/minimax.dart
+++ lib/utils/minimax.dart
@@ -15,7 +15,7 @@ class Minimax {
 
   int generateBestMove(BoardClass board) {
     int bestVal = minimax(board, 0, AI, -infinite, infinite);
-    print(bestVal);
+    print("bestVAl: "+bestVal.toString());
     return bestMove == -1 ? initMove : bestMove;
   }
 
@@ -24,6 +24,7 @@ class Minimax {
     int alternatePlayer = (currentPlayer == AI) ? HUMAN : AI;
 
     if (board.searchForMatch(5, alternatePlayer) > 0) {
+      print(currentPlayer);
       if (alternatePlayer == AI)
         return infinite - depth;
       else
@@ -38,71 +39,73 @@ class Minimax {
     List<int> changeList = [];
     List<bool> isExpanded = List(100);
 
+
     for (int i = 0; i < 100; i++) {
       isExpanded[i] = false;
     }
 
-    for (int i = 0; i < 10; i++) {
-      for (int j = 0; j < 10; j++) {
-        if (board.getPlayer(i, j) != -1) {
-          for (int it = 0; it < 8; it++) {
-            if ((i + dX[it] >= 0 && i + dX[it] < 10) &&
-                (j + dY[it] >= 0 && j + dY[it] < 10)) if (board.getPlayer(
-                i + dX[it], j + dY[it]) ==
-                -1) {
-              if (isExpanded[(i + dX[it]) * 10 + j + dY[it]] == false) {
-                isExpanded[(i + dX[it]) * 10 + j + dY[it]] = true;
-                blankTiles.add(board.copyBoard());
-                blankTiles[blankTiles.length - 1]
-                    .changeEntry(i + dX[it], j + dY[it], currentPlayer);
-                changeList.add((i + dX[it]) * 10 + j + dY[it]);
-              }
-            }
-          }
-        }
-      }
-    }
-
-//    for(int i=0; i<10; i++){
-//      for(int j=0; j<10; j++){
-//        if(board.getPlayer(i, j)==-1){
-//          blankTiles.add(board.copyBoard());
-//          blankTiles[blankTiles.length -1].changeEntry(i, j, currentPlayer);
-//          changeList.add(i*10 +j);
-//        }
-//
-//      }
-//    }
-
-
-
     if (currentPlayer == AI) {
       bestVal = -infinite;
 
-      for (int i = 0; i < blankTiles.length; i++) {
-        BoardClass tempBoard = blankTiles[i];
-        if (depth == 0) initMove = changeList[i];
-        bestVal =
-            max(bestVal, minimax(tempBoard, depth + 1, HUMAN, alpha, beta));
-        if (bestVal > alpha) {
-          alpha = bestVal;
-          bestMove = changeList[i];
+      for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+          if (board.getPlayer(i, j) != -1) {
+            for (int it = 0; it < 8; it++) {
+              if ((i + dX[it] >= 0 && i + dX[it] < 10) &&
+                  (j + dY[it] >= 0 && j + dY[it] < 10)) if (board.getPlayer(
+                  i + dX[it], j + dY[it]) ==
+                  -1) {
+                if (isExpanded[(i + dX[it]) * 10 + j + dY[it]] == false) {
+                  isExpanded[(i + dX[it]) * 10 + j + dY[it]] = true;
+
+                  BoardClass tempBoard = board.copyBoard();
+                  tempBoard.changeEntry(i + dX[it], j + dY[it], currentPlayer);
+//                  if (depth == 0) initMove = (i + dX[it]) * 10 + j + dY[it];
+                  bestVal = max(bestVal,
+                      minimax(tempBoard, depth + 1, HUMAN, alpha, beta));
+                  if (bestVal > alpha) {
+                    alpha = bestVal;
+                    bestMove = (i + dX[it]) * 10 + j + dY[it];
+                  }
+
+                  if (beta <= alpha) return bestVal;
+                }
+              }
+            }
+//            if (beta <= alpha) break;
+          }
         }
-
-        if (beta <= alpha) break;
+//        if (beta <= alpha) break;
       }
-      return bestVal;
     } else {
-      bestVal = infinite;
+      for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+          if (board.getPlayer(i, j) != -1) {
+            for (int it = 0; it < 8; it++) {
+              if ((i + dX[it] >= 0 && i + dX[it] < 10) &&
+                  (j + dY[it] >= 0 && j + dY[it] < 10)) if (board.getPlayer(
+                  i + dX[it], j + dY[it]) ==
+                  -1) {
+                if (isExpanded[(i + dX[it]) * 10 + j + dY[it]] == false) {
+                  isExpanded[(i + dX[it]) * 10 + j + dY[it]] = true;
 
-      for (BoardClass tempBoard in blankTiles) {
-        bestVal = min(bestVal, minimax(tempBoard, depth + 1, AI, alpha, beta));
-        beta = min(beta, bestVal);
-        if (beta <= alpha) break;
+                  bestVal = infinite;
+                  BoardClass tempBoard = board.copyBoard();
+                  tempBoard.changeEntry(i + dX[it], j + dY[it], currentPlayer);
+                  bestVal = min(
+                      bestVal, minimax(tempBoard, depth + 1, AI, alpha, beta));
+                  beta = min(beta, bestVal);
+                  if (beta <= alpha) return bestVal;
+                }
+              }
+//              if (beta <= alpha) break;
+            }
+          }
+        }
+//        if (beta <= alpha) break;
       }
-
-      return bestVal;
     }
+    return bestVal;
   }
 
   int cutOffEvaluation(BoardClass board, int currentPlayer ) {
@@ -114,8 +117,8 @@ class Minimax {
     value += sign*190500*board.searchForLooseEnds(4, 2, currentPlayer);
     value -= sign*190500*board.searchForLooseEnds(4, 2, alternatePlayer);
 
-    value += sign*75000*board.searchForLooseEnds(4, 1, currentPlayer);
-    value -= sign*75000*board.searchForLooseEnds(4, 1, alternatePlayer);
+    value += sign*95000*board.searchForLooseEnds(4, 1, currentPlayer);
+    value -= sign*95000*board.searchForLooseEnds(4, 1, alternatePlayer);
 
     value += sign*84000*board.searchForLooseEnds(3, 2, currentPlayer);
     value -= sign*84000*board.searchForLooseEnds(3, 2, alternatePlayer);
